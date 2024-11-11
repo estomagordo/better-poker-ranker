7ce00f2749e6455aed1b4225827dd9a2b0c52758
Christofer Ohlsson <christofer@miro.com>
2024-11-11 21:13:38 +0100

    a basic hand test

--- hand.py
+++ hand.py
@@ -6,27 +6,34 @@ class Hand:
     def __init__(self, ranking: RankingSchema, cards: list[Card]):
         self.ranking = ranking
         self.cards = sorted(cards, key=ranking.card_compare())
+        self.score = []
+
+    def hand_score(self):
+        if not self.score:
+            self.score = self.ranking.hand_score()(self.cards)
+
+        return self.score
 
     def __lt__(self, other):
-        return self.ranking.hand_score()(self.cards) < self.ranking.hand_score()(other.cards)
+        return self.hand_score() < other.hand_score()
     
     def __le__(self, other):
-        return self.ranking.hand_score()(self.cards) <= self.ranking.hand_score()(other.cards)
+        return self.hand_score() <= other.hand_score()
     
     def __gt__(self, other):
-        return self.ranking.hand_score()(self.cards) > self.ranking.hand_score()(other.cards)
+        return self.hand_score() > other.hand_score()
     
     def __ge__(self, other):
-        return self.ranking.hand_score()(self.cards) >= self.ranking.hand_score()(other.cards)
+        return self.hand_score() >= other.hand_score()
     
     def __eq__(self, other):
-        return self.ranking.hand_score()(self.cards) == self.ranking.hand_score()(other.cards)
+        return self.hand_score() == other.hand_score()
     
     def __ne__(self, other):
-        return self.ranking.hand_score()(self.cards) != self.ranking.hand_score()(other.cards)
+        return self.hand_score() != other.hand_score()
     
     def __str__(self):
-        return f'[{",".join(card for card in self.cards)}]'
+        return f'[{",".join(str(card) for card in self.cards)}]'
     
     def __repr__(self):
         return str(self)
--- test_hand.py
+++ test_hand.py
@@ -5,9 +5,7 @@ from standard_ranker import StandardRanker
 
 
 def test_straight_flush_beats_four_of_a_kind_with_standard_ranker():
-    handA = Hand(StandardRanker(), sample_hands.wheel_straight_flush)
-    handB = Hand(StandardRanker(), sample_hands.four_kings)
-
-    assert handA > handB
-
-test_straight_flush_beats_four_of_a_kind_with_standard_ranker()
+    hand_a = Hand(StandardRanker(), sample_hands.wheel_straight_flush)
+    hand_b = Hand(StandardRanker(), sample_hands.four_kings)
+    
+    assert hand_a > hand_b
