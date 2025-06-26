0850f63424aec430f1cf4e5e459af96137159a3f
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2025-06-26 14:26:30 +0200

    three tests ought to be enough for anyone

--- test_holdem.py
+++ test_holdem.py
@@ -14,3 +14,31 @@ def test_flush_over_flush():
     big_flush = Holdem(player_b, flop, turn, river)
 
     assert(big_flush > small_flush)
+
+
+def test_royal_on_board():
+    flop = [Card('Ad'), Card('Qd'), Card('Jd')]
+    turn = Card('Kd')
+    river = Card('Td')
+
+    player_a = [Card('Ts'), Card('3s')]
+    player_b = [Card('As'), Card('5s')]
+
+    a = Holdem(player_a, flop, turn, river)
+    b = Holdem(player_b, flop, turn, river)
+
+    assert(a == b)
+
+
+def test_one_liner():
+    flop = [Card('7s'), Card('9s'), Card('Td')]
+    turn = Card('Jc')
+    river = Card('Ah')
+
+    player_a = [Card('Ad'), Card('As')]
+    player_b = [Card('2s'), Card('8c')]
+
+    set = Holdem(player_a, flop, turn, river)
+    straight = Holdem(player_b, flop, turn, river)
+
+    assert(straight > set)
