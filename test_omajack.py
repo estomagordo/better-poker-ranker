1ccf77401c19e9285671b9e2df074ac2bbb50181
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2025-07-19 13:36:35 +0200

    this test makes more sense

--- test_omajack.py
+++ test_omajack.py
@@ -7,7 +7,7 @@ def test_rank_classify():
     turn = Card('9c')
     river = Card('9s')
 
-    hole_cards = [Card('Ad'), Card('Jh'), Card('Th'), Card('8s'), Card('7h')]
+    hole_cards = [Card('Th'), Card('8s'), Card('Jh'), Card('7h'), Card('Ad')]
 
     hand = Omajack(hole_cards, flop, turn, river)
 
