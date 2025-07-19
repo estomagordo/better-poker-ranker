4047493af6e2f336106b2a68daac955e62dc1567
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2025-07-19 13:16:22 +0200

    black jack scoring tests

--- test_black_jack_scorer.py
+++ test_black_jack_scorer.py
@@ -15,3 +15,18 @@ def test_scoring_simple():
     assert score([FIVE, NINE]) == 14
     assert score([NINE, FIVE]) == 14
     assert score([FIVE, KING]) == 15
+
+
+def test_scoring_aces():
+    assert score([ACE, DEUCE]) == 13
+    assert score([ACE, NINE]) == 20
+    assert score([ACE, TEN]) == 21
+    assert score([ACE, KING]) == 21
+    assert score([ACE, ACE, ACE, ACE, FIVE]) == 19
+    assert score([ACE, ACE, KING]) == 12
+
+
+def test_scoring_bust():
+    assert score([ACE, ACE, KING, KING]) == 0
+    assert score([DEUCE] * 11) == 0
+    assert score([NINE, NINE, FIVE]) == 0
